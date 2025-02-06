467aa6b7438db5e84b3331098cdae53627a5b16b
muhammadderic <muhammad.donny.e@gmail.com>
2025-02-06 20:50:39 +0700

    (feat) create post model, (refactor) update home route configuration

--- backend/post/models.py
+++ backend/post/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+  post_id = models.AutoField(primary_key=True)
+  title = models.CharField(max_length=100)
+  content = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
--- backend/post/urls.py
+++ backend/post/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import home
 
-urlPatterns = [
-  path('', views.index, name='home')
+urlpatterns = [
+  path('', home, name='home')
 ]
