825f7ec08b8b1569c7ab76db65bcab8fca72e351
muhammadderic <muhammad.donny.e@gmail.com>
2025-02-06 21:11:06 +0700

    (feat) create 'admin registration', (refactor) update gitignore

--- backend/post/admin.py
+++ backend/post/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+  list_display = ('post_id', 'title', 'content', 'created_at')
