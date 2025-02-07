95d4b09f231ab59c7986eb87c021ebc422ae1d15
muhammadderic <muhammad.donny.e@gmail.com>
2025-02-07 13:57:53 +0700

    (feat) create 'update' and 'delete' post functionalities

--- backend/post/urls.py
+++ backend/post/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home, add_post
+from .views import home, add_post, update_post, delete_post
 
 urlpatterns = [
   path('', home, name='home'),
-  path('add', add_post, name='add_post')
+  path('add', add_post, name='add_post'),
+  path('update/<int:post_id>', update_post, name='update_post'),
+  path('delete/<int:post_id>', delete_post, name='delete_post'),
 ]
--- backend/post/views.py
+++ backend/post/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Post
 
 def home(request):
@@ -15,3 +15,22 @@ def add_post(request):
     return redirect('home')
 
   return render(request, 'add_post.html')
+
+def update_post(request, post_id):
+  post = get_object_or_404(Post, post_id=post_id)
+
+  if (request.method == 'POST'):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    post.title = title
+    post.content = content
+    post.save()
+    return redirect('home')
+
+  return render(request, 'update_post.html', {'post': post})
+
+def delete_post(request, post_id):
+  post = get_object_or_404(Post, post_id=post_id)
+  post.delete()
+  return redirect('home')
