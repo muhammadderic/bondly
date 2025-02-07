3c875b99d646e656f06d3cb64125a687658a721b
muhammadderic <muhammad.donny.e@gmail.com>
2025-02-07 13:31:37 +0700

    (feat) create 'add post' functionality

--- backend/post/views.py
+++ backend/post/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Post
 
 def home(request):
   return render(request, 'home.html')
+
+def add_post(request):
+  if (request.method == 'POST'):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    post = Post(title=title, content=content)
+    post.save()
+    return redirect('home')
+
+  return render(request, 'add_post.html')
